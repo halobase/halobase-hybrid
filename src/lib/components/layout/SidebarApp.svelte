0cdf3ac254733b46cc11d6616fce6696dce26a83
mivinci <mivinci@qq.com>
2024-01-08 12:21:20 +0800

    :construction: fix styles

--- src/lib/components/layout/SidebarApp.svelte
+++ src/lib/components/layout/SidebarApp.svelte
@@ -18,8 +18,8 @@
             ><path
               fill="currentColor"
               d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
-            /></svg
-          >
+            />
+          </svg>
         </label>
       </div>
       <div class="sm:w-40 md:w-52 lg:w-64 xl:w-72 2xl:w-80 grow">
